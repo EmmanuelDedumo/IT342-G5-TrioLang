925650c097c626a1d78bb351f0d805720b53daf6
LAB14_USER ON WS22 <L14X08W22@citulab.net>
2025-02-12 09:19:01 +0800

    added a files in backend folder

---
+++ feature_backend/src/test/java/edu/cit/thrivewell/ThrivewellApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.thrivewell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrivewellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
